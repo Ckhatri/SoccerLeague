ba9da1b72983c4fd9fd94675f3b335ffc20444b2
ckhatri <chirag.k.khatri@gmail.com>
2015-07-27 14:51:45 -0500

    Added form to add teams on client side

--- client/views/teams.js
+++ client/views/teams.js
@@ -1,5 +1,33 @@
 Template.teams.helpers({
 	teams: function() {
 		return Teams.find();
+	},
+
+	isCreatingTeam: function() {
+		return Session.get('isCreatingTeam');
+	}
+});
+
+Template.teams.events({
+	'click .create': function(e, tpl){
+		event.preventDefault();
+		Session.set('isCreatingTeam', true);
+	},
+
+	'click .cancel': function(e, tpl){
+		event.preventDefault();
+		Session.set('isCreatingTeam', false);
+	},
+
+	'submit .create-team': function(event){
+		event.preventDefault();
+		var teamName = event.target.name.value;
+		Teams.insert({name: teamName}, function(error, _id){
+			if (error) {
+				alert(error);
+				Session.set('isCreatingTeam', true);
+			}
+		});
+		Session.set('isCreatingTeam', false);
 	}
 });
