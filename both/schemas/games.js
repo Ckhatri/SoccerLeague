250636df93133e3107f6d4b539be639bf00bb556
ckhatri <chirag.k.khatri@gmail.com>
2015-07-28 14:03:29 -0500

    Added a games schema

---
+++ both/schemas/games.js
@@ -0,0 +1,37 @@
+Games.attachSchema(new SimpleSchema({
+  completed: {
+    type: Boolean
+  },
+ 
+  ownerId: {
+    type: String
+  },
+  
+  teams: {
+    type: [Object]
+  },
+
+  createdAt: {
+    type: Date,
+    autoValue: function() {
+      if (this.isInsert) {
+        return new Date();
+      } else if (this.isUpsert) {
+        return {$setOnInsert: new Date()};
+      } else {
+        this.unset();
+      }
+    }
+  },
+  
+  updatedAt: {
+    type: Date,
+    autoValue: function() {
+      if (this.isUpdate) {
+        return new Date();
+      }
+    },
+    denyInsert: true,
+    optional: true
+  }
+}));
