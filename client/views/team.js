798af4fbecd05d8a639628991ab0a33bb7b796c0
ckhatri <chirag.k.khatri@gmail.com>
2015-07-28 12:42:39 -0500

    submitting the edit now calls the teamUpdate method

--- client/views/team.js
+++ client/views/team.js
@@ -6,29 +6,25 @@ Template.team.events({
   
 
   //since we're doing denormalization, if a name changes, we have to go into our Games and change that team's name
-  "submit form.form-edit": function(event, template){
-    event.preventDefault();
+  "submit form.form-edit": function(e, tpl){
+    e.preventDefault();
  
-    var teamName = template.$('input[name="name"]').val();
+    var teamName = tpl.$("input[name=name]").val();
     var self = this;
+
     if(teamName.length){
-      Teams.update(this._id, {$set: {name: teamName}}, function(error) {
-        if (!error) {
-          //get the games, go thorugh the games and then update the teamName. 
-          var games = Games.find({_id: {$in: self.gameIds}});
-          if (games.count()) {
-            _(games.fetch()).each(function(game) {
-              var team = _(game.teams).findWhere({_id: self._id});
-              if (team != null) {
-                team.name = teamName;
-                Games.update({_id: game._id}, {$set: {teams: game.teams}})
-              }
-            });
-          }
+      Meteor.call("teamUpdate", this._id, teamName, function(error){
+        if(error){
+          alert(error.reason);
+          Session.set('editedTeamId', self._id);
+          Tracker.afterFlush(function(){
+            tpl.$("input[name=name]").val(teamName);
+            tpl.$("input[name=name]").focus();
+          });
         }
       });
 
-      Session.set('editedTeamId', null);
+      Session.set('isEditingTeam', null);
     }
   },
  
