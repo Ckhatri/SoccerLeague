2b4c5e45dc47c61ba1f564d7ae4d3952e83b0883
ckhatri <chirag.k.khatri@gmail.com>
2015-07-28 13:01:17 -0500

    added subscriptions to wait on functions of router

--- both/router.js
+++ both/router.js
@@ -1,6 +1,16 @@
 Router.configure({
-	layoutTemplate: 'layout'
-})
+  layoutTemplate: 'layout'
+});
+ 
+Router.route('/', {
+  name: 'games',
+  waitOn: function(){
+    return [Meteor.subscribe("games"), Meteor.subscribe("teams")];
+  }
+});
 
-Router.route('/', {name: 'games'});
-Router.route('/teams');
+Router.route('/teams', {
+  waitOn: function(){
+    return Meteor.subscribe("teams");
+  }
+});
